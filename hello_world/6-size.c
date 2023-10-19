27a2e60d542b6955ac1da36667efb58df0b7b416
willybarreraroque <willy.br2002@gmail.com>
2023-10-19 07:03:05 -0700

    Task:06

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,11 +11,13 @@ int main(void)
 	char caracteres;
 	int entero;
 	long int entero_largo;
+	long long int entero_largo_largo;
 	float flotante;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(caracteres));
 	printf("Size of a int: %ld byte(s)\n", sizeof(entero));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(entero_largo));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(entero_largo_largo));
 	printf("Size of a float: %ld byte(s)\n", sizeof(flotante));
 	return (0);
 }
