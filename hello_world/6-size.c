2e0bdeaf133e1a07c46791cda77825f84f1b9d96
willybarreraroque <willy.br2002@gmail.com>
2023-10-19 06:35:55 -0700

    Task:06

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*
+ * main - Entry point
+ * Description: "Print size of variables"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char caracteres;
+	int entero;
+	long int entero_largo;
+	float flotante;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(caracteres));
+	printf("Size of a int: %ld byte(s)\n", sizeof(entero));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(entero_largo));	
+	printf("Size of a float: %ld byte(s)\n", sizeof(flotante));
+	return (0);
+}
