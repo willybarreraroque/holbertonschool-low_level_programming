a0a3da8323e97555bc1cb7499096109d12ea0745
willybarreraroque <willy.br2002@gmail.com>
2023-10-22 12:21:23 -0700

    Task:04

---
+++ variables_if_else_while/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - C program that prints letters and special characters.
+ * Description: followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
