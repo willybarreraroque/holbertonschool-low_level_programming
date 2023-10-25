c6eee435898a228712f249f661a0424917c83eb6
willybarreraroque <willy.br2002@gmail.com>
2023-10-24 20:48:11 -0700

    Task:0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,21 @@ int main(void)
 {
 	int n;
 
-	srand(time(NULL));
-	n=rand()%1000000000;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	srand(time(NULL));
-	printf("%d\n",rand());
+	if (n < 0)
+	{
+		printf("%d es negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
 
 }
