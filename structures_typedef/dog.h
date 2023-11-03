31e81f6b8da80a0c252f4341eeca1b80b8c63ffd
willybarreraroque <willy.br2002@gmail.com>
2023-11-03 09:51:53 -0700

    Task:0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Reali
+ * @name : define name
+ * @owner : define owner
+ * @age : define age
+ * Desciption: Define type struct
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
