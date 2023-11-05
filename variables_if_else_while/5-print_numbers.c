3260133f31ea902db3891714a95ae930f7ecc055
willybarreraroque <willy.br2002@gmail.com>
2023-11-04 21:53:49 -0700

    Task:05

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 /**
- * main - print number 0 to 9
- * Return : Always (0)
+ * main - Entry point
+ *
+ * Description: 'print number 0 to 9'
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
